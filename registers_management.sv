685804288c4b3c4b84b247bcfd054de8b4ef41b9
Tran Tai <trananhtai577@gmail.com>
2023-09-29 17:26:07 +0700

    Testing

--- registers_management.sv
+++ registers_management.sv
@@ -22,6 +22,7 @@ module registers_management
     input   wire                        main_program_state,        
     input   wire [REGISTER_WIDTH - 1:0] ra_register,
     output  wire                        synchronized_processors,
+    output  wire [0:REGISTER_AMOUNT - 1]processing_register_table,
     // To Processor & Multi-processor manager
     output  wire [REGISTER_WIDTH - 1:0] registers_renew         [0:REGISTER_AMOUNT - 1],
     
@@ -30,10 +31,9 @@ module registers_management
     
     reg [0:REGISTER_AMOUNT - 1] new_data_register_reg_main;
     reg [0:REGISTER_AMOUNT - 1] new_data_register_reg_sub;
+    reg [0:REGISTER_AMOUNT - 1] processing_register_table_reg;
     reg [1:0]                   processor_main_updater_state;
     reg [1:0]                   processor_sub_updater_state;
-    wire                        processor_idle_1_rising;
-    wire                        processor_idle_2_rising;
     reg [REG_CTN_WIDTH - 1:0]   register_num_buf_1;
     reg [REG_CTN_WIDTH - 1:0]   register_num_buf_2;
     reg                         synchronized_processor_1;
@@ -43,8 +43,10 @@ module registers_management
     
     localparam IDLE_STATE = 0;
     localparam UPDATING_STATE = 1;
+    localparam PRE_UPDATING_STATE = 3;
     localparam SYNCHRONIZATION_STATE = 2;
     
+    assign processing_register_table = processing_register_table_reg;
     for(genvar i = 0; i < REGISTER_AMOUNT; i = i + 1) begin
     if(i == 1) begin
         assign new_data_register[i] = 1;
@@ -58,21 +60,7 @@ module registers_management
     assign synchronization_processor_1 = synchronization_processor_1_reg;
     assign synchronization_processor_2 = synchronization_processor_2_reg;
     assign synchronized_processors = synchronized_processor_1 & synchronized_processor_2;
-    
-    edge_detector process_1_finish_detector
-                            (    
-                            .clk(clk),
-                            .sig_in(processor_idle_1),
-                            .out(processor_idle_1_rising),
-                            .rst_n(rst_n)
-                            );
-    edge_detector process_2_finish_detector
-                            (    
-                            .clk(clk),
-                            .sig_in(processor_idle_2),
-                            .out(processor_idle_2_rising),
-                            .rst_n(rst_n)
-                            );
+                           
                             
     // Processor 1 (main) updater
     always @(posedge clk, negedge rst_n) begin
@@ -81,13 +69,15 @@ module registers_management
             processor_main_updater_state <= IDLE_STATE;
             synchronized_processor_1 <= 1;
             synchronization_processor_1_reg <= 0;
+            processing_register_table_reg <= {32{1'b0}};
         end
         else begin
             case(processor_main_updater_state) 
                 IDLE_STATE: begin
                     if(boot_renew_register_1 == 1) begin
-                        processor_main_updater_state <= UPDATING_STATE;
+                        processor_main_updater_state <= PRE_UPDATING_STATE;
                         register_num_buf_1 <= register_num;
+                        processing_register_table_reg[register_num] <= 1'b1;
                         synchronized_processor_2 <= 0;
                     end
                     else begin
@@ -98,13 +88,21 @@ module registers_management
                         end
                     end
                 end
+                PRE_UPDATING_STATE: begin
+                    if(processor_idle_1 == 0) begin
+                        processor_main_updater_state <= UPDATING_STATE;
+                    end
+                    else processor_main_updater_state <= processor_main_updater_state;
+                end
                 UPDATING_STATE : begin
-                    if(processor_idle_1_rising) begin
+                    if(processor_idle_1) begin
                         processor_main_updater_state <= IDLE_STATE;
                         // Update data flag (change HIGH)
                         new_data_register_reg_main[register_num_buf_1] <= ~new_data_register_reg_sub[register_num_buf_1];
+                        // Update processor_register_table
+                        processing_register_table_reg[register_num_buf_1] <= 1'b0;
                     end
-                    else processor_main_updater_state <= UPDATING_STATE;
+                    else processor_main_updater_state <= processor_main_updater_state;
                 end
                 SYNCHRONIZATION_STATE: begin
                     processor_main_updater_state <= IDLE_STATE;
@@ -128,9 +126,10 @@ module registers_management
             case(processor_sub_updater_state) 
                 IDLE_STATE: begin
                     if(boot_renew_register_2 == 1) begin
-                        processor_sub_updater_state <= UPDATING_STATE;
+                        processor_sub_updater_state <= PRE_UPDATING_STATE;
                         register_num_buf_2 <= register_num;
                         synchronized_processor_1 <= 0;
+                        processing_register_table_reg[register_num] <= 1'b1;
                     end
                     else begin
                         if(synchronized_processor_2 == 0 & processor_main_updater_state == IDLE_STATE) begin
@@ -140,13 +139,21 @@ module registers_management
                         end
                     end
                 end
+                PRE_UPDATING_STATE: begin
+                    if(processor_idle_2 == 0) begin
+                        processor_sub_updater_state <= UPDATING_STATE;
+                    end
+                    else processor_sub_updater_state <= processor_sub_updater_state;
+                end 
                 UPDATING_STATE: begin
-                    if(processor_idle_2_rising) begin
+                    if(processor_idle_2) begin
                         processor_sub_updater_state <= IDLE_STATE;
                         // Update data flag (change LOW)
                         new_data_register_reg_sub[register_num_buf_2] <= new_data_register_reg_main[register_num_buf_2];
+                        // Update processor_register_table
+                        processing_register_table_reg[register_num_buf_2] <= 1'b0;
                     end
-                    else processor_sub_updater_state <= UPDATING_STATE;
+                    else processor_sub_updater_state <= processor_sub_updater_state;
                 end
                 SYNCHRONIZATION_STATE: begin
                     processor_sub_updater_state <= IDLE_STATE;
