f16ed372a52fa17651a03dd448cbdc1312151957
Tran Tai <trananhtai577@gmail.com>
2023-10-01 23:06:12 +0700

    Testing

--- Processor.sv
+++ Processor.sv
@@ -1,3 +1,4 @@
+`define DEBUG
 module Processor
     #(
     parameter           MAIN_RPOCESSOR          = 1,            // Main processor - Sub processor
@@ -56,22 +57,43 @@ module Processor
     parameter IMM_2_SPACE_WIDTH     = IMM_2_SPACE_MSB - IMM_2_SPACE_LSB + 1,
     parameter IMM_3_SPACE_MSB       = 31,
     parameter IMM_3_SPACE_LSB       = 27,
-    parameter IMM_3_SPACE_WIDTH     = IMM_3_SPACE_MSB - IMM_3_SPACE_LSB + 1,
-    
-    // Finish program condition 
-    parameter FINISH_PROGRAM_OPCODE  = 7'b0001011,      // Detect finish_program_opcode (this opcode is RESERVED)
-    parameter FINISH_PROGRAM_TIMER   = 125000,          // Counting time of EMPTY RX_module (assume 1ms)
-    
-    //ALU parameter 
-    parameter OPCODE_ALU_WIDTH      = 17, 
-    
+    parameter IMM_3_SPACE_WIDTH     = IMM_3_SPACE_MSB - IMM_3_SPACE_LSB + 1,// Opcode encoder
+    parameter JUMP_OFS_SPACE_MSB    = 31,
+    parameter JUMP_OFS_SPACE_LSB    = 7,
+    parameter JUMP_OFS_SPACE_WIDTH  = JUMP_OFS_SPACE_MSB - JUMP_OFS_SPACE_LSB + 1,// Opcode encoder
+    parameter OPT_SPACE_MSB         = 11,               // Option space
+    parameter OPT_SPACE_LSB         = 10,               // Option space    
+    parameter OPT_SPACE_WIDTH       = OPT_SPACE_MSB - OPT_SPACE_LSB + 1,
+    parameter IMM_4_SPACE_MSB       = 16,               // Option space
+    parameter IMM_4_SPACE_LSB       = 12,               // Option space    
+    parameter IMM_4_SPACE_WIDTH     = IMM_4_SPACE_MSB - IMM_4_SPACE_LSB + 1,
     // Opcode encoder
     parameter R_TYPE_ENCODE         = 7'b0110011,       // R-type
     parameter I_TYPE_ENCODE         = 7'b0010011,       // I-type
     parameter J_TYPE_ENCODE         = 7'b1100111,       // J-type
+    parameter JAL_TYPE_ENCODE       = 7'b1101111,       // J-type
+    parameter JALR_TYPE_ENCODE      = 7'b1101011,       // J-type
     parameter B_TYPE_ENCODE         = 7'b1100011,       // B-type   
     parameter LOAD_ENCODE           = 7'b0000011,       // LOAD-type
     parameter STORE_ENCODE          = 7'b0100011,       // STORE-type
+    parameter MISC_MEM_ENCODE       = 7'b0101111,       // Hardware-supportive memory type
+    parameter PROTOCOL_ENCODE       = 7'b1000001,       // Protocol-type
+    parameter SYSTEM_ENCODE         = 7'b1110111,       // System-type
+    // Funct3 enoder (for B-type)
+    parameter BEQ_ENCODE           = 3'b000,
+    parameter BNE_ENCODE           = 3'b001,
+    parameter BLT_ENCODE           = 3'b100,
+    parameter BGE_ENCODE           = 3'b101,
+    // Funct3 encoder (for SYSTEM-type) 
+    parameter BREAK_ENCODE         = 3'b001,            // BREAK: use for debugger
+    parameter EXIT_ENCODE          = 3'b010,
+    parameter RETI_ENCODE          = 3'b011,
+    // Funct3 encoder (for MISC-MEM type)
+    parameter FENCE_ENCODE         = 3'b010,
+    // Funct3 encoder (for Protocol-type)
+    parameter UART_ENCODE          = 3'b000,
+    parameter SPI_ENCODE           = 3'b010,
+    parameter I2C_ENCODE           = 3'b011,
     // Funct10 encoder
     parameter ADD_ENCODE            = 10'b0000000000,   
     parameter SUB_ENCODE            = 10'b1000000000,
@@ -95,7 +117,28 @@ module Processor
     // Funct3 encoder (for LOAD-type & STORE-type) 
     parameter BYTE_WIDTH_ENCODE     = 3'b000,  
     parameter WORD_WIDTH_ENCODE     = 3'b010,  
-    parameter DWORD_WIDTH_ENCODE    = 3'b011
+    parameter DWORD_WIDTH_ENCODE    = 3'b011,
+    
+    
+    // Finish program condition 
+    parameter FINISH_PROGRAM_OPCODE  = 7'b0001011,      // Detect finish_program_opcode (this opcode is RESERVED)
+    parameter FINISH_PROGRAM_TIMER   = 125000,          // Counting time of EMPTY RX_module (assume 1ms)
+    
+    // Protocol peripheral address encoder
+    parameter ADDR_MAPPING_PERIPHERAL   = 5, 
+    parameter NOTHING_MAPPING           = 0, 
+    parameter UART_TX_MAPPING           = 1, 
+    parameter UART_RX_MAPPING           = 2, 
+    parameter SPI_MAPPING               = 3, 
+    parameter I2C_MAPPING               = 4, 
+    parameter ADDDR_MAPPING_WIDTH       = $clog2(ADDR_MAPPING_PERIPHERAL),
+    // Protocol peripheral communication 
+    parameter AMOUNT_SND_BYTE           = 24,  
+    parameter AMOUNT_RCV_BYTE           = 16,
+    parameter AMOUNT_SND_WIDTH          = $clog2(AMOUNT_SND_BYTE),
+    parameter AMOUNT_RCV_WIDTH          = $clog2(AMOUNT_RCV_BYTE),
+    //ALU parameter 
+    parameter OPCODE_ALU_WIDTH      = 17
     )(
     input clk,
     
@@ -139,10 +182,15 @@ module Processor
     
     // For sub Processor ///////////////////////////////////////////////
     // Data_bus block
-    input   wire [DATA_WIDTH - 1:0]         data_bus_in_com_per,
-    output  wire [DATA_WIDTH - 1:0]         data_bus_out_com_per,
-    output  wire                            wr_use,
-    output  wire                            rd_use,
+    output  wire [ADDDR_MAPPING_WIDTH - 1:0]protocol_address_mapping,
+    output  wire [DOUBLEWORD_WIDTH*2 - 1:0] data_snd_protocol_per,
+    input   wire [DOUBLEWORD_WIDTH*3 - 1:0] data_rcv_protocol_per,
+    output  wire                            send_protocol_clk,
+    output  wire                            receive_protocol_clk,
+    output  wire [AMOUNT_SND_WIDTH - 1:0]   amount_snd_byte_protocol,
+    input   wire [AMOUNT_RCV_WIDTH - 1:0]   amount_rcv_byte_protocol,
+    input   wire                            snd_protocol_available,
+    input   wire                            rcv_protocol_available,
     //////////////////////////////////////////////////////////////////////
     // Register management
     output  wire [DOUBLEWORD_WIDTH - 1:0]   processor_registers [0:REGISTER_AMOUNT - 1],
@@ -151,10 +199,11 @@ module Processor
     
     input rst_n
     
-    
+    `ifdef DEBUG
     // Debug 
     ,output wire    [63:0]                      debug_1
     ,output wire    [63:0]                      debug_2
+    `endif
     );
     generate
     if(MAIN_RPOCESSOR == 1) begin   : MAIN_RPOCESSOR_BLOCK
@@ -226,6 +275,7 @@ module Processor
         localparam EXECUTE_RD_ACCESS_INSTRUCTION_STATE = 5;
         localparam EXECUTE_WR_PRE_ACCESS_INSTRUCTION_STATE = 6;
         localparam EXECUTE_WR_ACCESS_INSTRUCTION_STATE = 7;
+        localparam ADDRESS_MAPPING_PROT_STATE = 8;
         
         assign main_state = main_state_reg;
         assign RX_use_1 = RX_use_1_reg;
@@ -647,9 +697,10 @@ module Processor
                 end
             end
         end
-        
+        `ifdef DEBUG
         // Debug area
         assign debug_1 = {0, alu_result};
+        `endif
     end
     else begin                      : SUB_PROCESSOR_BLOCK
         reg [3:0]                           running_program_state_reg;
@@ -659,15 +710,25 @@ module Processor
         wire[OPCODE_SPACE_WIDTH - 1:0]      opcode_space;
         wire[FUNCT10_SPACE_WIDTH - 1:0]     funct10_space;
         wire[FUNCT3_SPACE_WIDTH - 1:0]      funct3_space;
-        wire[REGISTER_SPACE_WIDTH - 1:0]    rd_space;
+        wire[REGISTER_SPACE_WIDTH - 1:0]    rd1_space;
+        wire[REGISTER_SPACE_WIDTH - 1:0]    rd2_space;
+        wire[REGISTER_SPACE_WIDTH - 1:0]    rd3_space;
         wire[REGISTER_SPACE_WIDTH - 1:0]    rs1_space;
         wire[REGISTER_SPACE_WIDTH - 1:0]    rs2_space;
+        wire[REGISTER_SPACE_WIDTH - 1:0]    rs3_space;
         wire[IMM_1_SPACE_WIDTH - 1:0]       immediate_1_space;
         wire[IMM_2_SPACE_WIDTH - 1:0]       immediate_2_space;
         wire[IMM_3_SPACE_WIDTH - 1:0]       immediate_3_space;
+        wire[IMM_4_SPACE_WIDTH - 1:0]       immediate_4_space;
+        wire[OPT_SPACE_WIDTH - 1:0]         option_space;
         reg [DOUBLEWORD_WIDTH - 1:0]        rs1_buffer; 
         reg [DOUBLEWORD_WIDTH - 1:0]        rs2_buffer; 
-        
+        reg [DOUBLEWORD_WIDTH - 1:0]        rs3_buffer; 
+        // Protocol interface
+        reg [ADDDR_MAPPING_WIDTH - 1:0]     protocol_address_mapping_reg;
+        reg                                 send_protocol_clk_reg;
+        reg                                 receive_protocol_clk_reg;
+        reg [AMOUNT_SND_WIDTH - 1:0]        amount_snd_byte_protocol_reg;
         // Synchronization primitive
         //  - read 
         reg [ADDR_WIDTH_DM - 1:0]           addr_rd_reg;
@@ -698,6 +759,9 @@ module Processor
         localparam EXECUTE_RD_ACCESS_INSTRUCTION_STATE = 5;
         localparam EXECUTE_WR_PRE_ACCESS_INSTRUCTION_STATE = 6;
         localparam EXECUTE_WR_ACCESS_INSTRUCTION_STATE = 7;
+        localparam ADDRESS_MAPPING_PROT_STATE = 8;
+        localparam SEND_REQUEST_PROTOCOL_STATE = 9;
+        localparam RECEIVE_REQUEST_PROTOCOL_STATE = 10;
         
         // Synchronization primitive
         // -- read
@@ -714,19 +778,27 @@ module Processor
         assign opcode_space = fetch_instruction_reg[OPCODE_SPACE_MSB:OPCODE_SPACE_LSB];
         assign funct10_space = fetch_instruction_reg[FUNCT10_SPACE_MSB:FUNCT10_SPACE_LSB];
         assign funct3_space = fetch_instruction_reg[FUNCT3_SPACE_MSB:FUNCT3_SPACE_LSB];
-        assign rd_space = fetch_instruction_reg[RD_SPACE_MSB:RD_SPACE_LSB];
+        assign rd1_space = fetch_instruction_reg[RD_SPACE_MSB:RD_SPACE_LSB];
         assign rs1_space = fetch_instruction_reg[RS1_SPACE_MSB:RS1_SPACE_LSB];
         assign rs2_space = fetch_instruction_reg[RS2_SPACE_MSB:RS2_SPACE_LSB];
         assign immediate_1_space = fetch_instruction_reg[IMM_1_SPACE_MSB:IMM_1_SPACE_LSB];
         assign immediate_2_space = fetch_instruction_reg[IMM_2_SPACE_MSB:IMM_2_SPACE_LSB];
         assign immediate_3_space = fetch_instruction_reg[IMM_3_SPACE_MSB:IMM_3_SPACE_LSB];
+        assign immediate_4_space = fetch_instruction_reg[IMM_4_SPACE_MSB:IMM_4_SPACE_LSB];
+        assign rd2_space = rs1_space;
+        assign rd3_space = rs2_space;
+        assign rs3_space = rd1_space;
         // Multi-processor manager
         assign processor_idle = (running_program_state_reg == IDLE_STATE);
         // Register management
         for(genvar index_register = 0; index_register < REGISTER_AMOUNT; index_register = index_register + 1) begin
             assign processor_registers[index_register] = registers_owner[index_register];
         end
-        
+        // Protocol management 
+        assign data_snd_protocol_per = {rs3_buffer, rs2_buffer, rs1_buffer};
+        assign send_protocol_clk = send_protocol_clk_reg;
+        assign receive_protocol_clk = receive_protocol_clk_reg;
+        assign amount_snd_byte_protocol = amount_snd_byte_protocol_reg;
         // ALU block
         always @* begin
             case(opcode_space) 
@@ -803,6 +875,11 @@ module Processor
                 data_type_wr_reg <= 0;
                 rs1_buffer <= 0;
                 rs2_buffer <= 0;
+                rs3_buffer <= 0;
+                protocol_address_mapping_reg <= NOTHING_MAPPING;
+                send_protocol_clk_reg <= 0;
+                receive_protocol_clk_reg <= 0;
+                amount_snd_byte_protocol_reg <= 0;
             end
             else begin
                 case(running_program_state_reg) 
@@ -942,15 +1019,44 @@ module Processor
                                         alu_use <= 1;
                                         running_program_state_reg <= EXECUTE_WR_ADDR_INSTRUCTION_STATE;
                                     end
+                                    SYSTEM_ENCODE: begin
+                                    // Processor 2 doesn't process SYSTEM-type intruction
+                                    end
+                                    PROTOCOL_ENCODE: begin
+                                        running_program_state_reg <= ADDRESS_MAPPING_PROT_STATE;
+                                        case(funct3_space) 
+                                            UART_ENCODE: begin
+                                                if(option_space) begin  // TX
+                                                    protocol_address_mapping_reg <= UART_TX_MAPPING;
+                                                    // Confirm data
+                                                    rs1_buffer <= registers_renew[rs1_space];
+                                                    rs2_buffer <= registers_renew[rs2_space];
+                                                    rs3_buffer <= registers_renew[rs3_space];
+                                                end
+                                                else begin              // RX
+                                                    protocol_address_mapping_reg <= UART_RX_MAPPING;
+                                                end
+                                            end
+                                            SPI_ENCODE: begin
+                                                protocol_address_mapping_reg <= SPI_MAPPING;
+                                            end
+                                            I2C_ENCODE: begin
+                                                protocol_address_mapping_reg <= I2C_MAPPING;
+                                            end
+                                            default: begin
+                                            
+                                            end
+                                        endcase
+                                    end
                                 endcase 
                             end
                     EXECUTE_INSTRUCTION_STATE: begin
                                 alu_use <= 0;
                                 if(alu_idle == 1) begin
-                                    registers_owner[rd_space] <= alu_result;
+                                    registers_owner[rd1_space] <= alu_result;
                                     running_program_state_reg <= IDLE_STATE;
                                 end
-                                else running_program_state_reg <= EXECUTE_INSTRUCTION_STATE;
+                                else running_program_state_reg <= running_program_state_reg;
                             end
                     EXECUTE_ADDR_INSTRUCTION_STATE: begin
                                 alu_use <= 0;
@@ -966,7 +1072,7 @@ module Processor
                                     rd_finish_reg <= 0;      
                                     running_program_state_reg <= EXECUTE_RD_PRE_ACCESS_INSTRUCTION_STATE;
                                 end
-                                else running_program_state_reg <= EXECUTE_ADDR_INSTRUCTION_STATE;
+                                else running_program_state_reg <= running_program_state_reg;
                                 
                             end
                     EXECUTE_RD_PRE_ACCESS_INSTRUCTION_STATE: begin
@@ -974,15 +1080,15 @@ module Processor
                                     running_program_state_reg <= EXECUTE_RD_ACCESS_INSTRUCTION_STATE;
                                     rd_ins_reg <= 0;
                                 end
-                                else running_program_state_reg <= EXECUTE_RD_PRE_ACCESS_INSTRUCTION_STATE;
+                                else running_program_state_reg <= running_program_state_reg;
                             end
                     EXECUTE_RD_ACCESS_INSTRUCTION_STATE: begin
                                 if(rd_idle) begin
-                                    registers_owner[rd_space] <= data_bus_rd;
+                                    registers_owner[rd1_space] <= data_bus_rd;
                                     rd_finish_reg <= 1;
                                     running_program_state_reg <= IDLE_STATE;
                                 end
-                                else running_program_state_reg <= EXECUTE_RD_ACCESS_INSTRUCTION_STATE;
+                                else running_program_state_reg <= running_program_state_reg;
                             end
                     EXECUTE_WR_ADDR_INSTRUCTION_STATE: begin
                                 alu_use <= 0;
@@ -998,19 +1104,52 @@ module Processor
                                     endcase
                                     running_program_state_reg <= EXECUTE_WR_PRE_ACCESS_INSTRUCTION_STATE;
                                 end
-                                else running_program_state_reg <= EXECUTE_WR_ADDR_INSTRUCTION_STATE;
+                                else running_program_state_reg <= running_program_state_reg;
                             end
                     EXECUTE_WR_PRE_ACCESS_INSTRUCTION_STATE: begin
                                 if(wr_access) begin
                                     running_program_state_reg <= EXECUTE_WR_ACCESS_INSTRUCTION_STATE;
                                 end
-                                else running_program_state_reg <= EXECUTE_WR_PRE_ACCESS_INSTRUCTION_STATE;
+                                else running_program_state_reg <= running_program_state_reg;
                             end
                     EXECUTE_WR_ACCESS_INSTRUCTION_STATE: begin  
                                 // Notation of EXECUTE_WR_ACCESS_INSTRUCTION_STATE: Just wait for 1 cycle to confirm stablization of interactive buffer
                                 running_program_state_reg <= IDLE_STATE;
                                 wr_ins_reg <= 0;
                             end
+                    ADDRESS_MAPPING_PROT_STATE: begin
+                        running_program_state_reg <= SEND_REQUEST_PROTOCOL_STATE;
+                        case(protocol_address_mapping_reg) 
+                            UART_TX_MAPPING: begin
+                                if(snd_protocol_available) begin
+                                    running_program_state_reg <= IDLE_STATE;
+                                    send_protocol_clk_reg <= 1;
+                                    amount_snd_byte_protocol_reg <= immediate_4_space;
+                                end
+                            end
+                            UART_RX_MAPPING: begin
+                                if(rcv_protocol_available) begin
+                                    running_program_state_reg <= IDLE_STATE;
+                                    receive_protocol_clk_reg <= 1;
+                                    registers_owner[rd1_space] <= data_rcv_protocol_per[63:0];
+                                    registers_owner[rd2_space] <= data_rcv_protocol_per[127:64];
+                                    registers_owner[rd3_space] <= amount_rcv_byte_protocol;
+                                end
+                            end
+                            SPI_MAPPING: begin
+                            
+                            end
+                            I2C_MAPPING: begin
+                            
+                            end
+                        endcase 
+                    end
+                    SEND_REQUEST_PROTOCOL_STATE: begin
+                    
+                    end
+                    RECEIVE_REQUEST_PROTOCOL_STATE: begin
+                    
+                    end            
                 endcase
             end
         end
@@ -1022,6 +1161,10 @@ module Processor
                 end
             end
         end
+        `ifdef DEBUG
+        // Debug area
+        assign debug_2 = {0, 0};
+        `endif
     end
     
     
