976baf285d8ec45e63842841b2168cde618dca73
Tran Tai <trananhtai577@gmail.com>
2023-10-29 15:53:08 +0700

    Add 1 more read_handler to DATA_MEMORY

--- ram.sv
+++ ram.sv
@@ -1,15 +1,19 @@
-//`define DEBUG
+`define DEBUG
 //`define RESERVED_REG_DEBUG
-// Byte order: Little Endian
+// Byte order:    Little Endian
+// Acess option:  1)  1wr-1rd
+//                2)  1wr-2rd (set DUAL_READ_HANDLER = 1)
 module ram
     #(
         parameter DATA_WIDTH        = 8,
         parameter BYTE_WIDTH        = 8,
         parameter WORD_WIDTH        = BYTE_WIDTH*4,
         parameter DOUBLEWORD_WIDTH  = BYTE_WIDTH*8,
-        parameter ADDR_DEPTH        = 2048,  // <=> 512 bytes
+        parameter ADDR_DEPTH        = 1024,  // <=> 512 bytes
         parameter ADDR_WIDTH        = $clog2(ADDR_DEPTH),
         
+        // Read handler
+        parameter DUAL_READ_HANDLER = 0,    // Increase power consumption
         // Deep configuartion
         parameter DEFAULT_ADDR  = 0,
         parameter DEFAULT_DATA  = 0,
@@ -50,25 +54,31 @@ module ram
     input   wire                        clk,
     
     input   wire    [DOUBLEWORD_WIDTH - 1:0]    data_bus_wr,
-    output  logic   [DOUBLEWORD_WIDTH - 1:0]    data_bus_rd,
+    output  logic   [DOUBLEWORD_WIDTH - 1:0]    data_bus_rd_1,
+    output  logic   [DOUBLEWORD_WIDTH - 1:0]    data_bus_rd_2,
     
     
-    input   wire    [DATA_TYPE_WIDTH - 1:0]     data_type_rd,
+    input   wire    [DATA_TYPE_WIDTH - 1:0]     data_type_rd_1,
+    input   wire    [DATA_TYPE_WIDTH - 1:0]     data_type_rd_2,
     input   wire    [DATA_TYPE_WIDTH - 1:0]     data_type_wr,
     
-    input   wire    [ADDR_WIDTH - 1:0]  addr_rd,
-    input   wire    [ADDR_WIDTH - 1:0]  addr_wr,
+    input   wire    [ADDR_WIDTH - 1:0]          addr_rd_1,
+    input   wire    [ADDR_WIDTH - 1:0]          addr_rd_2,
+    input   wire    [ADDR_WIDTH - 1:0]          addr_wr,
     
-    input   wire                        rd_ins,
-    input   wire                        wr_ins,
+    input   wire                                rd_en_1,
+    input   wire                                rd_en_2,
+    input   wire                                wr_ins,
     
     // Flag read 
-    output  wire                        invalid_rd_flag,
-    output  wire                        invalid_wr_flag,
+    output  wire                                invalid_rd_flag_1,
+    output  wire                                invalid_rd_flag_2,
+    output  wire                                invalid_wr_flag,
     
     // State
-    output  wire                        rd_idle,
-    output  wire                        wr_idle,
+    output  wire                                rd_idle_1,
+    output  wire                                rd_idle_2,
+    output  wire                                wr_idle,
     
     // Optional (to save configuration data)
     output  wire    [DATA_WIDTH - 1:0]  reserved_registers  [0:RESERVED_REG_AMOUNT - 1],
@@ -88,42 +98,42 @@ module ram
     //  + IO: 1.938W
     //  + Static: 0.162W
     
-//    wire    [ADDR_WIDTH - 1:0]  addr_rd;
+//    wire    [ADDR_WIDTH - 1:0]  addr_rd_1;
 //    wire    [ADDR_WIDTH - 1:0]  addr_wr;
-//    assign addr_rd = 27;
+//    assign addr_rd_1 = 27;
 //    assign addr_wr = 02;
-    
-    // Set of registers
-    reg     [DATA_WIDTH - 1:0]      registers [0: ADDR_DEPTH - 1];
-    
-    reg     [ADDR_WIDTH - 1:0]      addr_rd_buf;
-    reg     [ADDR_WIDTH - 1:0]      addr_wr_buf;
-    
-    reg     [DATA_TYPE_WIDTH - 1:0] data_type_rd_buf;
-    logic   [DATA_TYPE_WIDTH - 1:0] data_type_wr_buf;
-    wire                            data_type_dword_en;
-    wire                            data_type_word_en;
-    wire                            data_type_byte_en;
-    wire                            data_type_dword_word_en;
-    
-    reg [1:0] state_counter_rd;
-    reg [1:0] state_counter_wr;
-    
-    wire rd_ins_sync;
-    wire wr_ins_sync;
-    
-    reg [DOUBLEWORD_WIDTH - 1:0] data_bus_wr_buf;
-    
-    reg wr_clk_en;         // Write clock 
-    wire [ADDR_WIDTH - 1:0] locate_addr_wr  [0:ADDR_DEPTH - 1];
-    wire                    reg_clk         [0:ADDR_DEPTH - 1];
-    
+
     // Common state 
     localparam INIT_STATE = 3;
     localparam IDLE_STATE = 0;
     localparam LOAD_ADDR_STATE = 1;
     // Write state
     localparam WRITE_STATE = 2;
+    // Address encode doubleword
+    localparam ADDR_DWORD_ENCODE = $clog2(ADDR_DEPTH / 8);
+    // Address encode word
+    localparam ADDR_WORD_ENCODE  = $clog2(ADDR_DEPTH / 4);
+    // Address encode byte
+    localparam ADDR_BYTE_ENCODE  = $clog2(ADDR_DEPTH);
+    
+    
+    // Set of registers
+    reg     [DATA_WIDTH - 1:0]      registers [0: ADDR_DEPTH - 1];
+    
+    reg     [ADDR_WIDTH - 1:0]      addr_wr_buf;
+    reg     [DOUBLEWORD_WIDTH - 1:0]data_bus_wr_buf;
+    logic   [DATA_TYPE_WIDTH - 1:0] data_type_wr_buf;
+    
+    wire                            data_type_dword_en;
+    wire                            data_type_word_en;
+    wire                            data_type_byte_en;
+    wire                            data_type_dword_word_en;
+    
+    reg [1:0] state_counter_wr;
+    
+    reg wr_clk_en;         // Write clock 
+    wire [ADDR_WIDTH - 1:0] locate_addr_wr  [0:ADDR_DEPTH - 1];
+    wire                    reg_clk         [0:ADDR_DEPTH - 1];
     
     // Reserved register management 
     genvar index_reserved_reg;
@@ -133,83 +143,22 @@ module ram
         end
     endgenerate
     
-    // Read management
-    reg rd_occupy_start;
-    reg rd_occupy_stop;
-    wire rd_occupied;
+    assign data_type_dword_en = (data_type_wr_buf == DATA_TYPE_DOUBLEWORD_ENCODE);
+    assign data_type_word_en  = (data_type_wr_buf == DATA_TYPE_WORD_ENCODE);
+    assign data_type_byte_en  = (data_type_wr_buf == DATA_TYPE_BYTE_ENCODE);
+    assign data_type_dword_word_en  = data_type_word_en | data_type_dword_en;
     
     
-    assign rd_occupied = rd_occupy_start ^ rd_occupy_stop;
-    assign rd_idle = ~rd_occupied;
-    
-    always @(posedge rd_ins, negedge rst_n) begin
-        if(!rst_n) rd_occupy_start <= 0;
-        else rd_occupy_start <= ~rd_occupy_stop;
-    end
-    
-    edge_detector rd_sig_management
-                            (    
-                            .clk(clk),
-                            .sig_in(rd_ins),
-                            .out(rd_ins_sync),
-                            .rst_n(rst_n)
-                            );
-    always @(posedge clk, negedge rst_n) begin
-        if(!rst_n) begin
-            state_counter_rd <= IDLE_STATE;
-            addr_rd_buf <= DEFAULT_ADDR;
-            rd_occupy_stop <= 0;
-        end
-        else begin
-            case(state_counter_rd)
-                IDLE_STATE: begin
-                    if(rd_ins_sync) begin
-                        state_counter_rd <= LOAD_ADDR_STATE;
-                        addr_rd_buf <= addr_rd;
-                        data_type_rd_buf <= data_type_rd;
-                    end
-                end
-                LOAD_ADDR_STATE: begin
-                    state_counter_rd <= IDLE_STATE;
-                    rd_occupy_stop <= rd_occupy_start;
-                end
-                default: state_counter_rd <= IDLE_STATE;
-            endcase
-        end
-    end
-    // Declare logic
+    // Aligment block Index 
     wire [DOUBLEWORD_WIDTH - 1:0] data_bus_dword_rd [0:ADDR_DEPTH/8 - 1];
     wire [WORD_WIDTH - 1:0]       data_bus_word_rd [0:ADDR_DEPTH/4 - 1];
     wire [BYTE_WIDTH - 1:0]       data_bus_byte_rd [0:ADDR_DEPTH/1 - 1];
-    wire [DOUBLEWORD_WIDTH - 1:0] dword_mulitplexer;
-    wire [DOUBLEWORD_WIDTH - 1:0] word_mulitplexer;
-    wire [DOUBLEWORD_WIDTH - 1:0] byte_mulitplexer;
-    // Address encode doubleword
-    localparam ADDR_DWORD_ENCODE = $clog2(ADDR_DEPTH / 8);
-    wire [ADDR_DWORD_ENCODE - 1:0] address_rd_dword_encode;
-    wire                           invalid_rd_dword_flag;
-    // Address encode word
-    localparam ADDR_WORD_ENCODE  = $clog2(ADDR_DEPTH / 4);
-    wire [ADDR_WORD_ENCODE - 1:0] address_rd_word_encode;
-    wire                          invalid_rd_word_flag;
-    // Address encode byte
-    localparam ADDR_BYTE_ENCODE  = $clog2(ADDR_DEPTH);
-    wire [ADDR_BYTE_ENCODE - 1:0] address_rd_byte_encode;
-    
-    assign address_rd_dword_encode = addr_rd_buf[ADDR_WIDTH - 1: 3];    // Index of double word 
-    assign address_rd_word_encode  = addr_rd_buf[ADDR_WIDTH - 1: 2];    // Index of word
-    assign address_rd_byte_encode  = addr_rd_buf;                       // Index of byte
-    
-    assign invalid_rd_flag       = invalid_rd_dword_flag | invalid_rd_word_flag;
-    assign invalid_rd_dword_flag = (data_type_rd_buf == DATA_TYPE_DOUBLEWORD_ENCODE) ? {addr_rd_buf[2:0] != 3'b000} : 1'b0;       // Valid alignment (divisible by 8)
-    assign invalid_rd_word_flag  = (data_type_rd_buf == DATA_TYPE_WORD_ENCODE)       ? {addr_rd_buf[1:0] != 2'b00}  : 1'b0;         // Valid alignment (divisible by 8)
-    
     // Byte order: Little endian (MSB is at highest addr)
     // Double word multiplexer (8bytes)
     for(genvar rd_index_dword = 0; rd_index_dword < ADDR_DEPTH / 8; rd_index_dword = rd_index_dword + 1) begin
         assign data_bus_dword_rd[rd_index_dword] = {registers[rd_index_dword*8 + 7], registers[rd_index_dword*8 + 6], registers[rd_index_dword*8 + 5], registers[rd_index_dword*8 + 4], registers[rd_index_dword*8 + 3], registers[rd_index_dword*8 + 2], registers[rd_index_dword*8 + 1], registers[rd_index_dword*8 + 0]};
     end
-    // Word multiplexer (4bytes)
+        // Word multiplexer (4bytes)
     for(genvar rd_index_word = 0; rd_index_word < ADDR_DEPTH / 4; rd_index_word = rd_index_word + 1) begin
         assign data_bus_word_rd[rd_index_word] = {registers[rd_index_word*4 + 3], registers[rd_index_word*4 + 2], registers[rd_index_word*4 + 1], registers[rd_index_word*4 + 0]};
     end
@@ -217,23 +166,86 @@ module ram
     for(genvar rd_index_byte = 0; rd_index_byte < ADDR_DEPTH / 1; rd_index_byte = rd_index_byte + 1) begin
         assign data_bus_byte_rd[rd_index_byte] = registers[rd_index_byte];
     end
+    // Read handler 1
+    wire    [ADDR_WIDTH - 1:0]      addr_rd_internal_1;
+    reg     [DATA_TYPE_WIDTH - 1:0] data_type_rd_internal_1;
+    assign rd_idle_1 = 1'b1;
+    assign addr_rd_internal_1      = (rd_en_1) ? addr_rd_1 : 0;
+    assign data_type_rd_internal_1 = (rd_en_1) ? data_type_rd_1 : 0;
+    // Multiplexer
+    wire [DOUBLEWORD_WIDTH - 1:0] dword_mulitplexer_1;
+    wire [DOUBLEWORD_WIDTH - 1:0] word_mulitplexer_1;
+    wire [DOUBLEWORD_WIDTH - 1:0] byte_mulitplexer_1;
     
-    assign dword_mulitplexer = data_bus_dword_rd[address_rd_dword_encode];
-    assign word_mulitplexer = {32'b00, data_bus_word_rd[address_rd_word_encode]};
-    assign byte_mulitplexer = {56'b00, data_bus_byte_rd[address_rd_byte_encode]};
+    // Alignment
+    wire [ADDR_DWORD_ENCODE - 1:0]  address_rd_dword_encode_1;
+    wire                            invalid_rd_dword_flag_1;
+    wire [ADDR_WORD_ENCODE - 1:0]   address_rd_word_encode_1;
+    wire                            invalid_rd_word_flag_1;
+    wire [ADDR_BYTE_ENCODE - 1:0]   address_rd_byte_encode_1;
+        
+    assign address_rd_dword_encode_1 = addr_rd_internal_1[ADDR_WIDTH - 1: 3];    // Index of double word 
+    assign address_rd_word_encode_1  = addr_rd_internal_1[ADDR_WIDTH - 1: 2];    // Index of word
+    assign address_rd_byte_encode_1  = addr_rd_internal_1;                       // Index of byte
+        
+    assign invalid_rd_flag_1        = invalid_rd_dword_flag_1 | invalid_rd_word_flag_1;
+    assign invalid_rd_dword_flag_1  = (data_type_rd_internal_1 == DATA_TYPE_DOUBLEWORD_ENCODE) ? {addr_rd_internal_1[2:0] != 3'b000} : 1'b0;       // Valid alignment (divisible by 8)
+    assign invalid_rd_word_flag_1   = (data_type_rd_internal_1 == DATA_TYPE_WORD_ENCODE)       ? {addr_rd_internal_1[1:0] != 2'b00}  : 1'b0;         // Valid alignment (divisible by 8)
+        
+    assign dword_mulitplexer_1 = data_bus_dword_rd[address_rd_dword_encode_1];
+    assign word_mulitplexer_1  = {32'b00, data_bus_word_rd[address_rd_word_encode_1]};
+    assign byte_mulitplexer_1  = {56'b00, data_bus_byte_rd[address_rd_byte_encode_1]};
     always_comb begin
-        case(data_type_rd_buf)
-            DATA_TYPE_DOUBLEWORD_ENCODE: begin
-                data_bus_rd = dword_mulitplexer;
-            end
-            DATA_TYPE_WORD_ENCODE: begin
-                data_bus_rd = word_mulitplexer;
-            end
-            DATA_TYPE_BYTE_ENCODE: begin
-                data_bus_rd = byte_mulitplexer;
-            end
-            default: data_bus_rd = 64'b00;
+        case(data_type_rd_internal_1)
+            DATA_TYPE_DOUBLEWORD_ENCODE: data_bus_rd_1 = dword_mulitplexer_1;
+            DATA_TYPE_WORD_ENCODE: data_bus_rd_1 = word_mulitplexer_1;
+            DATA_TYPE_BYTE_ENCODE: data_bus_rd_1 = byte_mulitplexer_1;
+            default: data_bus_rd_1 = {DOUBLEWORD_WIDTH{1'b0}};
         endcase 
+    end   
+    if(DUAL_READ_HANDLER == 0) begin                        : SINGLE_READ_HANDLER_BLOCK
+        assign invalid_rd_flag_2        = 1'b0;
+        assign rd_idle_2                = 1'b0;
+        assign data_bus_rd_2            = {DOUBLEWORD_WIDTH{1'b0}};
+    end
+    else begin                                              : DUAL_READ_HANDLER_BLOCK
+        // Read handler 2
+        assign rd_idle_2               = 1'b1;
+        wire    [ADDR_WIDTH - 1:0]      addr_rd_internal_2;
+        reg     [DATA_TYPE_WIDTH - 1:0] data_type_rd_internal_2;
+        assign addr_rd_internal_2      = (rd_en_2) ? addr_rd_2 : 0;
+        assign data_type_rd_internal_2 = (rd_en_2) ? data_type_rd_2 : 0;
+        // Multiplexer
+        wire [DOUBLEWORD_WIDTH - 1:0] dword_mulitplexer_2;
+        wire [DOUBLEWORD_WIDTH - 1:0] word_mulitplexer_2;
+        wire [DOUBLEWORD_WIDTH - 1:0] byte_mulitplexer_2;
+        
+        // Alignment
+        wire [ADDR_DWORD_ENCODE - 1:0]  address_rd_dword_encode_2;
+        wire                            invalid_rd_dword_flag_2;
+        wire [ADDR_WORD_ENCODE - 1:0]   address_rd_word_encode_2;
+        wire                            invalid_rd_word_flag_2;
+        wire [ADDR_BYTE_ENCODE - 1:0]   address_rd_byte_encode_2;
+        
+        assign address_rd_dword_encode_2 = addr_rd_internal_2[ADDR_WIDTH - 1: 3];    // Index of double word 
+        assign address_rd_word_encode_2  = addr_rd_internal_2[ADDR_WIDTH - 1: 2];    // Index of word
+        assign address_rd_byte_encode_2  = addr_rd_internal_2;                       // Index of byte
+        
+        assign invalid_rd_flag_2        = invalid_rd_dword_flag_2 | invalid_rd_word_flag_2;
+        assign invalid_rd_dword_flag_2  = (data_type_rd_internal_2 == DATA_TYPE_DOUBLEWORD_ENCODE) ? {addr_rd_internal_2[2:0] != 3'b000} : 1'b0;       // Valid alignment (divisible by 8)
+        assign invalid_rd_word_flag_2   = (data_type_rd_internal_2 == DATA_TYPE_WORD_ENCODE)       ? {addr_rd_internal_2[1:0] != 2'b00}  : 1'b0;         // Valid alignment (divisible by 8)
+        
+        assign dword_mulitplexer_2 = data_bus_dword_rd[address_rd_dword_encode_2];
+        assign word_mulitplexer_2  = {32'b00, data_bus_word_rd[address_rd_word_encode_2]};
+        assign byte_mulitplexer_2  = {56'b00, data_bus_byte_rd[address_rd_byte_encode_2]};
+        always_comb begin
+            case(data_type_rd_internal_2)
+                DATA_TYPE_DOUBLEWORD_ENCODE: data_bus_rd_2 = dword_mulitplexer_2;
+                DATA_TYPE_WORD_ENCODE: data_bus_rd_2 = word_mulitplexer_2;
+                DATA_TYPE_BYTE_ENCODE: data_bus_rd_2 = byte_mulitplexer_2;
+                default: data_bus_rd_2 = {DOUBLEWORD_WIDTH{1'b0}};
+            endcase 
+        end
     end
     
     // Write management 
@@ -247,15 +259,7 @@ module ram
     always @(posedge wr_ins, negedge rst_n) begin
         if(!rst_n) wr_occupy_start <= 0;
         else wr_occupy_start <= ~wr_occupy_stop;
-    end 
-    
-    edge_detector wr_sig_management
-                            (    
-                            .clk(clk),
-                            .sig_in(wr_ins),
-                            .out(wr_ins_sync),
-                            .rst_n(rst_n)
-                            );             
+    end       
     
     wire                           invalid_wr_dword_flag;
     wire                           invalid_wr_word_flag;
@@ -263,11 +267,6 @@ module ram
     assign invalid_wr_flag       =      invalid_wr_dword_flag | invalid_wr_word_flag;             
     assign invalid_wr_dword_flag =      (data_type_wr_buf == DATA_TYPE_DOUBLEWORD_ENCODE) ? {addr_wr_buf[2:0] != 3'b000} : 1'b0;                   
     assign invalid_wr_word_flag  =      (data_type_wr_buf == DATA_TYPE_WORD_ENCODE)       ? {addr_wr_buf[1:0] != 2'b00}  : 1'b0;                   
-    
-    assign data_type_dword_en = (data_type_wr_buf == DATA_TYPE_DOUBLEWORD_ENCODE);
-    assign data_type_word_en  = (data_type_wr_buf == DATA_TYPE_WORD_ENCODE);
-    assign data_type_byte_en  = (data_type_wr_buf == DATA_TYPE_BYTE_ENCODE);
-    assign data_type_dword_word_en  = data_type_word_en | data_type_dword_en;
     
     always @(posedge clk, negedge rst_n) begin
         if(!rst_n) begin
@@ -281,7 +280,7 @@ module ram
             wr_clk_en <= 0;
             case(state_counter_wr) 
                 IDLE_STATE: begin
-                    if(wr_ins_sync) begin
+                    if(wr_ins) begin
                         state_counter_wr <= LOAD_ADDR_STATE;
                         addr_wr_buf <= addr_wr;
                         data_bus_wr_buf <= data_bus_wr;
